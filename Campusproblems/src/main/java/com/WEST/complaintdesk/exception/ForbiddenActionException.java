67127422fdbcc3da1c5babe28425f562ca325769
aliza20-hub <aliza2020.q@gmail.com>
2026-08-14 15:45:47 +0530

    .

--- Campusproblems/src/main/java/com/WEST/complaintdesk/exception/ForbiddenActionException.java
+++
@@ -1,7 +0,0 @@
-package com.WEST.complaintdesk.exception;
-
-public class ForbiddenActionException extends RuntimeException {
-    public ForbiddenActionException(String message) {
-        super(message);
-    }
-}
